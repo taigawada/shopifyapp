404c5072d4474339411379d6e82f313d8819a881
TaigaWada <athtela2266@gmail.com>
2023-02-03 20:33:45 +0900

    first commit

--- web/frontend/assets/index.ts
+++ web/frontend/assets/index.ts
@@ -1,4 +1,3 @@
 export { default as notFoundImage } from './empty-state.svg';
 export { default as trophyImage } from './home-trophy.png';
 export { default as mailPrintImage } from './mail-print.png';
-export { sampleData } from './sample-data';
